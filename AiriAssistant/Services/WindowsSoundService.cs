0bb8cf287673f8fd9518f0db6fffcd9d051f6333
TuanAnhKazuto <tuananhkazuto@gmail.com>
2026-06-06 15:26:30 +0700

    make notification  when change connect device

---
+++ AiriAssistant/Services/WindowsSoundService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Win32;
+
+namespace AiriAssistant.Services
+{
+    public class WindowsSoundService
+    {
+        private const string BasePath =
+            @"AppEvents\Schemes\Apps\.Default";
+
+        public void DisableSound()
+        {
+            DisableSoundEvent("DeviceConnect");
+            DisableSoundEvent("DeviceDisconnect");
+        }
+
+        private void DisableSoundEvent(string eventName)
+        {
+            string path = $"{BasePath}\\{eventName}\\.Current";
+
+            using RegistryKey? key = Registry.CurrentUser.OpenSubKey(path, writable: true);
+
+            if (key == null) return;
+
+            key.SetValue("", "");
+        }
+    }
+}
